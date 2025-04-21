eb4ba57a4d04cdb5f754a7651ca5ca6e8657d277
Leandro <menoci.leandro@gmail.com>
2025-04-20 22:36:41 -0700

    HashSet

--- Set/src/Main.java
+++ Set/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        
+//        Set<User> users = new HashSet<>();
+
+//        Set<User> users = new TreeSet<>(((u1, u2) -> {
+//            var compareResult = 0;
+//            if (u1.getId() < u2.getId()) compareResult --;
+//            if (u1.getId() > u2.getId()) compareResult ++;
+//            return compareResult;
+//        }));
+
+        Set<User> users = new TreeSet<>(Comparator.comparingInt(User::getId));
+        users.add(new User(1, "Joao"));
+        users.add(new User(2, "Maria"));
+        users.add(new User(3, "Juca"));
+        users.add(new User(4, "Monica"));
+
+//        System.out.println(users.contains(new User(1, "Joao")));
+//
+//        users.forEach(System.out::println);
+
+        System.out.println(users);
+
     }
 }
