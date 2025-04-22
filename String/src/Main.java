52fe8c0021c9dad5b233422ca7776a04f797d19a
Leandro <menoci.leandro@gmail.com>
2025-04-21 21:32:59 -0700

    String

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        var value = "java;java;java;java";
+//        value = value.replaceFirst("j", "J");
+//        value = value.replace("j", "J");
+
+//        var values = value.split(";");
+//        for (var v : values){
+//            System.out.println(v);
+//        }
+
+        var value = """
+                {"name":"Joao","age":"18"}""";
+        Map<String, String> map = new HashMap<>();
+        value = value.replace("{", "").replace("}", "").replace("\"", "");
+        var valueArr = value.split(",");
+        for (var v : valueArr) {
+            var keyValue = v.split(":");
+            map.put(keyValue[0], keyValue[1]);
+        }
+        System.out.println(map);
     }
 }
