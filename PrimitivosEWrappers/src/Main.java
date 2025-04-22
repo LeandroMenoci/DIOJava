43345eac05732aed869ed3dea0be37e2ad20019e
Leandro <menoci.leandro@gmail.com>
2025-04-21 20:59:09 -0700

    Wrappers

--- PrimitivosEWrappers/src/Main.java
+++ PrimitivosEWrappers/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        var user = new User("Joao", 20);
+        System.out.println(user);
+        printValue(user);
+        System.out.println(user);
+    }
+
+    private static void printValue(final User user) {
+//        user = new User("Maria", 33); sem o final no parametro
+        user.setName("Maria");
+        user.setAge(33);
+        System.out.println(user);
+
     }
 }
