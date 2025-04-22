880b5b462fcaaa511ae7847a1da73fba6d8d6817
Leandro <menoci.leandro@gmail.com>
2025-04-21 21:59:33 -0700

    StringBuilder, StringBuffer

--- StringBuilderAndStringBuffer/src/Main.java
+++ StringBuilderAndStringBuffer/src/Main.java
@@ -1,5 +1,40 @@
+import java.time.Duration;
+import java.time.OffsetDateTime;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        var builder = new StringBuilder("123456789");
+        System.out.println(builder.delete(0, 3));
+
+
+//        var stringStart = OffsetDateTime.now();
+//        String stringConcat = "";
+//        for (int i = 0; i < 400_000; i++) {
+//            stringConcat += i;
+//        }
+//        var stringEnd = OffsetDateTime.now();
+//        System.out.printf("String: %s \n", Duration.between(stringStart,
+//                stringEnd).toMillis());
+
+//        var stringBuilderStart = OffsetDateTime.now();
+//        StringBuilder builderConcat = new StringBuilder();
+//        for (int i = 0; i < 400_000; i++) {
+//            builderConcat.append(i);
+//        }
+//        var stringBuilderEnd = OffsetDateTime.now();
+//        System.out.printf("StringBuilder (singlethread): %s \n",
+//                Duration.between(stringBuilderStart,
+//                stringBuilderEnd).toMillis());
+//
+//        var stringBufferStart = OffsetDateTime.now();
+//        StringBuffer bufferConcat = new StringBuffer();
+//        for (int i = 0; i < 400_000; i++) {
+//            bufferConcat.append(i);
+//        }
+//        var stringBufferEnd = OffsetDateTime.now();
+//        System.out.printf("StringBuffer (Multithread): %s \n", Duration.between(stringBufferStart,
+//                stringBufferEnd).toMillis());
     }
 }
