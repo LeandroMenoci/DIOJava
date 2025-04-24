89c69cd9c4842b9e598985a9ac438ba34456a638
Leandro <menoci.leandro@gmail.com>
2025-04-23 21:27:51 -0700

    BigDecimal

---
+++ BigDecimal/src/Main.java
@@ -0,0 +1,45 @@
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        BigDecimal value1 = new BigDecimal("0.1");
+        BigDecimal value2 = new BigDecimal("0.2");
+
+        System.out.println(value1.multiply(value2));
+
+
+
+//        System.out.println();
+//        System.out.println(value1.add(value2));
+//        System.out.println("---=-=-=---=");
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".1")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".2")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".3")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".4")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".5")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".6")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".7")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".8")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1" +".9")));
+//        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("2")));
+
+
+
+//        var a)lue1 = 0.1f;
+//        var value2 = 0.2f;
+//        System.out.println(value1 + value2);
+//        System.out.println("---=-=-=---=");
+//        System.out.println(2.00f - 1.1f);
+//        System.out.println(2.00f - 1.2f);
+//        System.out.println(2.00f - 1.3f);
+//        System.out.println(2.00f - 1.4f);
+//        System.out.println(2.00f - 1.5f);
+//        System.out.println(2.00f - 1.6f);
+//        System.out.println(2.00f - 1.7f);
+//        System.out.println(2.00f - 1.8f);
+//        System.out.println(2.00f - 1.9f);
+//        System.out.println(2.00f - 2f);
+
+
+    }
+}
