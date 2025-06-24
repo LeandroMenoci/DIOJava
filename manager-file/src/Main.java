1fd65993aa5ff5ccdf0e9894c1e72cb98ed47683
Leandro <menoci.leandro@gmail.com>
2025-06-23 17:36:31 -0700

    Finalizando implementação java.io

--- manager-file/src/Main.java
+++ manager-file/src/Main.java
@@ -7,7 +7,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         FilePersistence persistence = new IOFilePersistence("user.csv");
-        System.out.println(persistence.write("Lucas;lucas@lucas.com;15/02/1992"));
+        System.out.println("====================");
+        System.out.println(persistence.write("Lucas;lucas@lucas.com;15/02/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.write("Maria;maria@maria.com;12/02/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.write("Leandro;leandro@leandro.com;11/02/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.write("Bete;bete@bete.com;30/05/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.write("Bia;bia@bia.com;25/11/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.findAll());
+        System.out.println("====================");
+        System.out.println(persistence.remove("Bia;"));
+        System.out.println("====================");
+        System.out.println(persistence.remove("Carlos;"));
+        System.out.println("====================");
+        System.out.println(persistence.findBy("Lucas;"));
+        System.out.println("====================");
+        System.out.println(persistence.findBy("leandro@"));
+        System.out.println("====================");
+        System.out.println(persistence.findBy("25/11/1992;"));
+        System.out.println("====================");
+        System.out.println(persistence.replace("@maria.com;12/02", "Claudia;claudia@claudia.com;21/03/1992"));
+        System.out.println("====================");
+        System.out.println(persistence.findAll());
 
     }
 }
