83dd679f628f022c28b0d17a9f56582998b7a617
Leandro <menoci.leandro@gmail.com>
2025-06-30 12:01:08 -0700

    iniciando UI

---
+++ Board/src/main/java/br/com/dio/service/BoardService.java
@@ -0,0 +1,51 @@
+package br.com.dio.service;
+
+import br.com.dio.persistence.dao.BoardColumnDAO;
+import br.com.dio.persistence.dao.BoardDAO;
+import br.com.dio.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public BoardEntity insert(BoardEntity entity) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
+        try {
+            dao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+                c.setBoard(entity);
+                return c;
+            }).toList();
+            for (var column : columns) {
+                boardColumnDAO.insert(column);
+            }
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        }
+
+        return entity;
+    }
+
+    public boolean delete(Long id) throws SQLException{
+        var dao = new BoardDAO(connection);
+        try{
+            if (!dao.exists(id)) {
+                return false;
+            }
+            dao.delete(id);
+            connection.commit();
+            return true;
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        }
+    }
+}
