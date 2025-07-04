287421059140822e18ec52b1d0349468e6c40f75
Leandro <menoci.leandro@gmail.com>
2025-07-03 22:32:09 -0700

    Annotattion Processor

---
+++ annotation-processor/processor/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "br.com.dio"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("com.google.auto.service:auto-service:1.1.1")
+    annotationProcessor("com.google.auto.service:auto-service:1.1.1")
+    implementation("com.squareup:javapoet:1.13.0")
+
+    compileOnly(project(":annotation"))
+    implementation(project(":annotation"))
+
+}
---
+++ annotation-processor/sample/src/main/java/br/com/dio/Main.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.PersonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        var person = new PersonBuilder()
+                .id(1)
+                .name("Leandro")
+                .build();
+        System.out.println(person);
+
+    }
+}
